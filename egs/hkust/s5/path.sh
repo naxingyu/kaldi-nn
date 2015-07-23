65616a810631280cb0fb07e2315421ba432f400b
naxingyu <asr.naxingyu@gmail.com>
2015-07-23 11:33:45 +0800

    add egs

---
+++ egs/hkust/s5/path.sh
@@ -0,0 +1,5 @@
+export KALDI_TOLD=/nobackup/f1/software/kaldi/kaldi-trunk
+export KALDI_ROOT=/nobackup/s1/asr/naxingyu/tools/kaldi-trunk-5125
+export KALDI_TEST=/nobackup/s1/asr/naxingyu/tools/kaldi-trunk-dev
+export PATH=$PWD/utils/:$KALDI_ROOT/src/bin:$KALDI_TOLD/tools/openfst/bin:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lm/:$KALDI_ROOT/src/sgmmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$KALDI_TEST/src/nnetbin:$KALDI_TEST/src/nnet2bin:$KALDI_ROOT/src/online2bin/:$KALDI_ROOT/src/ivectorbin/:$KALDI_ROOT/src/lmbin/:$PWD:$PATH
+export LC_ALL=C
