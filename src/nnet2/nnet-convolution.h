7c26d1da7f28a69f4533f71df4fedc1ad3ea2ceb
naxingyu <asr.naxingyu@gmail.com>
2015-07-25 15:25:46 +0800

    add conlution component in nnet2

---
+++ src/nnet2/nnet-convolution.h
@@ -0,0 +1,36 @@
+// nnet2/nnet-convolution.h
+
+// Copyright 2015  Xingyu Na
+
+// See ../../COPYING for clarification regarding multiple authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+// WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+// See the Apache 2 License for the specific language governing permissions and
+// limitations under the License.
+
+
+#ifndef KALDI_NNET2_NNET_CONVOLUTION_H_
+#define KALDI_NNET2_NNET_CONVOLUTION_H_
+
+#include "base/kaldi-common.h"
+#include "matrix/matrix-lib.h"
+#include "cudamatrix/cu-matrix-lib.h"
+
+#include <iostream>
+
+namespace kaldi {
+namespace nnet2 {
+
+} // namespace nnet2
+} // namespace kaldi
+
+#endif
